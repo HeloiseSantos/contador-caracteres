1345d5811e5a62deff89d0182cdac6d7f37cb4ba
Heloíse Silva Santos <51417819+HeloiseSantos@users.noreply.github.com>
2022-02-19 13:06:11 -0300

    Cria contador de caracteres e palavras (#2)

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$('textarea').on('keyup paste', function() {
+    setTimeout(function() {
+        var valorTextarea = $('textarea').val().length,
+        palavras = $('textarea').val().split(' ').length;
+    
+        $('.caracteres').text(valorTextarea);
+        $('.palavras').text(palavras);
+    }, 200);
+});
